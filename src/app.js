6387e62c696f163fdaabc8c38b390a1ff35d45c3
jaellomiya <jaellomiya@gmail.com>
2023-12-20 17:45:50 +0200

    Added basic JavaScript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-alert("hello")
+function refreshWeather(response) {
+    let tepereatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "de8fb83a5518a504d62a04oadf7b9fta";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+
+searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
